f43150b1cde33aad9cc6fe252547de9b3682fefb
Wieland Schöbl <wulkanat@gmail.com>
2022-01-31 15:30:10 +0100

    binary packaging

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,1 +1,4 @@
-export {};
+import { Server } from 'socket.io';
+export declare const io: Server<import("socket.io/dist/typed-events").DefaultEventsMap, import("socket.io/dist/typed-events").DefaultEventsMap, import("socket.io/dist/typed-events").DefaultEventsMap, any>;
+export declare const users: import("socket.io").Namespace<import("socket.io/dist/typed-events").DefaultEventsMap, import("socket.io/dist/typed-events").DefaultEventsMap, import("socket.io/dist/typed-events").DefaultEventsMap, any>;
+export declare const controllers: import("socket.io").Namespace<import("socket.io/dist/typed-events").DefaultEventsMap, import("socket.io/dist/typed-events").DefaultEventsMap, import("socket.io/dist/typed-events").DefaultEventsMap, any>;
